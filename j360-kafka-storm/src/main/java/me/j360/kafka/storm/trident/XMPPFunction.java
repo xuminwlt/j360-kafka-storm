a13f428b8b879a295df91c8536deb3f403f1925d
min_xu <xumin_wlt@163.com>
2016-07-29 10:57:30 +0800

    add  xmpp pom

--- j360-kafka-storm/src/main/java/me/j360/kafka/storm/trident/XMPPFunction.java
+++ j360-kafka-storm/src/main/java/me/j360/kafka/storm/trident/XMPPFunction.java
@@ -1,9 +1,20 @@
 package me.j360.kafka.storm.trident;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.packet.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import storm.trident.operation.BaseFunction;
+import storm.trident.operation.TridentCollector;
+import storm.trident.operation.TridentOperationContext;
 import storm.trident.tuple.TridentTuple;
+
+import javax.security.sasl.SaslException;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Package: me.j360.kafka.storm.trident
@@ -27,24 +38,30 @@ public class XMPPFunction extends BaseFunction {
 
     @Override
     public void prepare(Map conf,
-                        TridentOperationContextcontext) {
+                        TridentOperationContext context) {
         LOG.debug("Prepare: {}",conf);
         super.prepare(conf, context);
         this.to = (String) conf.get(XMPP_TO);
 
         ConnectionConfiguration config = new
                 ConnectionConfiguration((String)  conf.get(XMPP_SERVER));
-        this.xmppConnection = newXMPPConnection(config);
+        this.xmppConnection = new XMPPConnection(config);
         try {
             this.xmppConnection.connect();
             this.xmppConnection.login((String)conf.get(XMPP_USER),
                     (String)conf.get(XMPP_PASSWORD));
         } catch (XMPPException e) {
             LOG.warn("Error initializingXMPP Channel", e);
+        } catch (SaslException e) {
+            e.printStackTrace();
+        } catch (SmackException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
     public void execute(TridentTuple tuple,
-                        TridentCollectorcollector) {
+                        TridentCollector collector) {
         Message msg = new Message(this.to,Message.Type.normal);
         msg.setBody(this.mapper.toMessageBody(tuple));
         this.xmppConnection.sendPacket(msg);
