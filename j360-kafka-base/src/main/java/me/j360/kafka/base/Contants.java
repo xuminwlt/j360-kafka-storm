71a95f16fce607783366b2f93c66ec510ea2f066
min_xu <xumin_wlt@163.com>
2017-08-05 17:51:20 +0800

    add kafka for spark

--- j360-kafka-base/src/main/java/me/j360/kafka/base/Contants.java
+++ j360-kafka-base/src/main/java/me/j360/kafka/base/Contants.java
@@ -1,17 +1,16 @@
 package me.j360.kafka.base;
 
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Contants {
 
 
     //kafka
     public final static class TOPIC {
-        public final static String Send = "topic_send";
+        public final static String Send = "zipkin";
+        public final static String DMP = "dmp";
+        public final static String DMP1 = "dmp-1";
     }
-    public final static String kafkaUrl = "172.16.10.113:9092";
-    public final static String zkUrl = "172.16.10.113:2181";
+    public final static String kafkaUrl = "localhost:9092";
+    public final static String zkUrl = "172.16.10.201:2181";
 
 }
--- j360-kafka-base/src/main/java/me/j360/kafka/base/newversion/KafkaProductDMPExample.java
+++ j360-kafka-base/src/main/java/me/j360/kafka/base/newversion/KafkaProductDMPExample.java
@@ -13,7 +13,7 @@ import java.util.Properties;
  * Date: 16/8/2 下午5:51
  * 说明：
  */
-public class KafkaProductExample {
+public class KafkaProductDMPExample {
 
     public static void main(String args[]){
         Properties props = new Properties();
@@ -26,10 +26,10 @@ public class KafkaProductExample {
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        String sss = "In Kafka the communication between the clients and the servers is done with a simple, high-performance, language agnostic TCP protocol. This protocol is versioned and maintains backwards compatibility with older version. We provide a Java client for Kafka, but clients are available in many languages.";
+        String sss = "{\"uid\":3,\"device_id\":\"068b746ed4620d25e26055a9f804385f\",\"vp_id\":5,\"event_time\":1430204612405,\"os_type\":\"Android\",\"play_count\":6}";
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
         for(int i = 0; i < 1000; i++)
-            producer.send(new ProducerRecord<String, String>(Contants.TOPIC.Send, Integer.toString(0), sss));
+            producer.send(new ProducerRecord<String, String>(Contants.TOPIC.DMP, Integer.toString(0), sss));
 
         producer.close();
     }
